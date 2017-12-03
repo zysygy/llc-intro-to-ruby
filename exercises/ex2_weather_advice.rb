fcbaf7e8e4614469d933369868fdde4913ea30b8
Allen <allen@Allens-MacBook-Pro.local>
2017-12-03 16:11:41 -0500

    Updated broken links, tweaked some slides

--- exercises/ex2_weather_advice.rb
+++ exercises/ex2_weather_advice.rb
@@ -1,13 +1,17 @@
 # Ask the user for input on the weather!
-
+puts "What is the weather?"
 
 # Use chomp to get the user input
-
+weather = gets.chomp
 
 # If the weather is "cold"...
-
-
+if weather == "cold"
+  puts "wear a parka"
 # If the weather is "raining"...
-
+elsif weather == "raining"
+  puts "bring an umbrella. ella"
 
 # If it is neither...
+else
+  puts "Have a nice day"
+end
--- exercises/ex3_smarter_weather.rb
+++ exercises/ex3_smarter_weather.rb
@@ -1,16 +1,17 @@
 # Ask the user for input on the weather!
-
+puts "What's the weather?"
 # Use chomp to get the user input
-
+weather = gets.chomp
+weather = weather.to_i
 # Instead of telling your program if it's cold,
 # you can input the temperature and let it decide what to do.
 
 # if the weather is greater than or equal to 25 degrees
-if "replace this with the right condition"
+if weather >= 25
     print "Go to the beach!"
 # the weather is less than 25 degrees AND greater than 15 degrees
-elsif "replace this with the right conditions"
-    # Still warm enough for ice cream!
+elsif weather > 15 and weather < 25
+    print "Still warm enough for ice cream!"
 else
-    # Wear a sweater and dream of beaches.
+    print "Wear a sweater and dream of beaches."
 end
