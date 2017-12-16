5f1c1c0a5f7b8bde4d22dc76e50aa799f66b0855
Allen <allen@Allens-MacBook-Pro.local>
2017-12-15 21:44:56 -0500

    Reworked core slides

--- hello.rb
+++
@@ -1,9 +0,0 @@
-# Ask user for input
-print "Please enter your name: "
-
-# Assign the input value to a variable
-# Your program waits for the user to hit enter
-name = gets.chomp
-
-# ... then continues to this line
-puts "Hello " + name
